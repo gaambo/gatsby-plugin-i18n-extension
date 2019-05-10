0902b35bc9af46c78e04d6339acf8d43f1a78fe2
Fabian Todt <fabian@fabiantodt.at>
2019-05-10 17:34:42 +0200

    add filter for language menu creation of available pages

--- src/util.js
+++ src/util.js
@@ -13,16 +13,21 @@ const getLanguageMenu = (
   currentLanguage,
   currentUrl,
   languages,
-  defaultLanguage
+  defaultLanguage,
+  allPages = null
 ) => {
   const homeLink = `/${currentLanguage}/`.replace(`/${defaultLanguage}/`, "/"); // removes default language key because home url in default language is only /
-  return getLangs(
+  const links = getLangs(
     languages,
     currentLanguage,
     langKey => getUrlForLang(homeLink, currentUrl, langKey) // get's called inside map in getLangs
   ).map(item => ({
     ...item,
-    link: item.link.replace(`/${defaultLanguage}`, `/`)
+    link: item.link.replace(`/${defaultLanguage}/`, `/`)
   }));
+  if (allPages) {
+    return links.filter(link => allPages.includes(link.link));
+  }
+  return links;
 };
 export { getLanguageMenu };
